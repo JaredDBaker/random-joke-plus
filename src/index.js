6a3a5002d6487fc1a6d53a125b30d8a33b840a46
Jared Baker <jaredbaker2018@gmail.com>
2021-02-17 12:11:49 -0500

    finished phase 2

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 console.log('First web service starting up ...');
 
-const htmlHandler = require("./htmlResponse.js");
-const jsonHandler = require("./jsonResponses.js");
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponse.js');
+const jsonHandler = require('./jsonResponses.js');
 
 // 1 - pull in the HTTP server module
-const http = require('http');
 
 // 2 - pull in URL and query modules (for URL parsing)
-const url = require('url');
 
 // 3 - locally this will be 3000, on Heroku it will be assigned
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-
 const urlStruct = {
-  '/random-joke' : jsonHandler.getRandomJokeResponse,
-  notFound : htmlHandler.get404Response,
+  '/random-joke': jsonHandler.getRandomJokeResponse,
+  notFound: htmlHandler.get404Response,
 };
 
-//test commit comment
+// test commit comment
 // 7 - this is the function that will be called every time a client request comes in
 // this time we will look at the `pathname`, and send back the appropriate page
 // note that in this course we'll be using arrow functions 100% of the time in our server-side code
@@ -30,8 +30,11 @@ const onRequest = (request, response) => {
   // console.log("parsedUrl=", parsedUrl);
   // console.log("pathname=", pathname);
 
+  const params = query.parse(parsedUrl.query);
+  console.log(params.limit);
+
   if (urlStruct[pathname]) {
-    urlStruct[pathname](request, response);
+    urlStruct[pathname](request, response, params);
   } else {
     urlStruct.notFound(request, response);
   }
